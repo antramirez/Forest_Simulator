d43febd9113705630cc7347aee9a69dbb5d1f5ea
Anthony Ramirez <ar4477@nyu.edu>
2018-11-26 20:49:52 -0500

    add comments

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+// require express and make an app
 const express = require('express');
 const app = express();
-
+// require path
 const path = require('path');
 
+// add middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
+// listen on port 3000
 app.listen(3000);
